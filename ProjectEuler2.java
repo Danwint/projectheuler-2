7c3aa28c246cd19e92ad941b8ea9445402868382
Daniel Wintrich <danielwintrich@Daniels-MacBook-Pro.local>
2015-09-30 14:44:42 -0400

    Fibonacci sequence activate!

--- ProjectEuler2.java
+++ ProjectEuler2.java
@@ -1,6 +1,14 @@
 public class ProjectEuler2 {
 	public static void main(String[] args) {
-		
+		int fibo1 = 1;
+		int fibo2 = 2;
+		int fibo3 = 3;
+		for (int i = 0; fibo3 < 4000000; i++) {
+			fibo3 = fibo1 + fibo2;
+			fibo1 = fibo2;
+			fibo2 = fibo3;
+			System.out.println(fibo3);
+		}
 		
 	}
 }
