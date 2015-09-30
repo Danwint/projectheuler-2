96a3c6de396f60b30cc8c0dfd6663e02914eeb55
Seth Pinckney <sethpinckney@Seths-MacBook-Pro.local>
2015-09-30 15:00:30 -0400

    Here you go, I think this is done.

--- ProjectEuler2.java
+++ ProjectEuler2.java
@@ -1,14 +1,20 @@
 public class ProjectEuler2 {
 	public static void main(String[] args) {
-		int fibo1 = 1;
-		int fibo2 = 2;
+		int fibo1 = 0;
+		int fibo2 = 1;
 		int fibo3 = 3;
+		int fibosum = 0;
 		for (int i = 0; fibo3 < 4000000; i++) {
+			
 			fibo3 = fibo1 + fibo2;
 			fibo1 = fibo2;
 			fibo2 = fibo3;
-			System.out.println(fibo3);
+			
+			if(fibo3 % 2 == 0){
+				
+				fibosum = fibosum + fibo3;
+				System.out.println(fibosum);
+			}
 		}
-		
 	}
 }
